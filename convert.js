ab527050bcf2dd78b647fe52379ca702ee965dbf
5a6io <yhm54321@naver.com>
2025-02-14 23:15:45 +0900

    notion-to-md

--- convert.js
+++ convert.js
@@ -1,51 +1,22 @@
 const fs = require('fs');
 const { Client } = require('@notionhq/client');
 const execSync = require('child_process').execSync;
-const notion2md = require('notion2md');
+const { NotionToMarkdown } = require('notion-to-md');
 
 require('dotenv').config();
 const notion = new Client({auth : process.env.NOTION_API_KEY});
-
-async function getDatabasePages(databaseId) {
-    const response = await notion.databases.query({
-        database_id: databaseId,
-    })
-
-    return response.results;
-}
-
-async function convertNotionPageToMD(pageId) {
-    try {
-      const markdownContent = await notion2md(pageId);
-      return markdownContent;
-    } catch (error) {
-      console.error('Error converting page to MD:', error);
-      return null;
+const n2m = new NotionToMarkdown({ 
+    notionClient: notion,
+    config: {
+        seperateChildPage: true,
     }
-}
+});
+const databaseId = process.env.DATABASE_ID;
 
-function getPageTitle(page){
-    const titleProperty = page.properties.title;
-    return titleProperty ? titleProperty.title[0].text.content : 'Untitled';
-}
   
-// 데이터베이스 내 페이지들을 순차적으로 변환
-async function convertDatabaseToMarkdown(databaseId) {
-const pages = await getDatabasePages(databaseId);
-  
-    for (const page of pages) {
-      const pageId = page.id;
-      const pageTitle = getPageTitle(page);
-      const markdownContent = await convertNotionPageToMD(pageId);
-  
-      if (markdownContent) {
-        fs.writeFileSync(`./markdown_pages/${pageTitle}.md`, markdownContent);
-      }
-    }
-  
-    console.log('모든 페이지를 Markdown으로 변환 완료!');
-}
-  
-// 실행: 데이터베이스 ID를 입력하세요
-const databaseId = process.env.DATABASE_ID; // 여기에 데이터베이스 ID 입력
-convertDatabaseToMarkdown(databaseId);
+(async () => {
+    const mdblocks = await n2m.pageToMarkdown("target_page_id");
+    const mdString = n2m.toMarkdownString(mdblocks);
+    
+    console.log(mdString);
+})();
