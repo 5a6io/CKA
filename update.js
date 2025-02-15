1c0cf5fb2e0707d0885622f92c7e7ec14fa2903d
5a6io <yhm54321@naver.com>
2025-02-15 23:41:12 +0900

    update

--- update.js
+++ update.js
@@ -7,8 +7,7 @@ const notion = new Client({auth : process.env.NOTION_API_KEY});
 const databaseId = process.env.DATABASE_ID;
   
 (async () => {
-    try {
-        const response = await notion.databases.query({
+    const response = await notion.databases.query({
         database_id: databaseId,
         sorts: [
             {
@@ -16,40 +15,44 @@ const databaseId = process.env.DATABASE_ID;
                 direction: 'ascending',
             },
         ],
-      });
+    });
 
-      let mdContent = `# 🌟CKA(Certified Kubernetes Administrator)\n
-      ## ✍🏻Summarize Lecture
-      I summarized the lecture with watching videos on 'Certified Kubernetes Administrator(CKA) with Practice Test.
+    let mdContent = `# 🌟CKA(Certified Kubernetes Administrator)
+    ## ✍🏻Summarize Lecture
+    I summarized the lecture with watching videos on 'Certified Kubernetes Administrator(CKA) with Practice Test.
       
-      <table>
-      <thead>
+    <table>
+    <thead>
         <tr>
             <th>Section</th>
             <th>:black_square_button:</th>
         </tr>
-      </thead>
-      <tbody>
-      `;
+    </thead>
+    <tbody>`;
 
-      const pages = response.results.map(page => {
-        const name = page.properties?.Name?.title?.[0]?.text?.content;
-        const checkbox = page.properties?.Checkbox?.checkbox;
+    const pages = response.results.map(page => {
+    const name = page.properties?.Name?.title?.[0]?.text?.content;
+    const checkbox = page.properties?.Checkbox?.checkbox;
       
-        return {
-          name: name,
-          checkbox: checkbox
+    return {
+            name: name,
+            checkbox: checkbox
         };
-      });
+    });
 
-      for (let page of pages){
-        mdContent += `<tr><td>${page.name}</td>`;
-        mdContent += (page.checkbox == true) ? `<td><p>U+2705</p></td></tr>` : `<td></td></tr>`;
-      }
-      mdContent += `</tbody></table>`;
-      
-      fs.writeFileSync("README.md", mdContent, "utf8");
-    } catch (error){
-        console.error("다음과 같은 오류 발생:", error);
+    for (let page of pages){
+        mdContent += `
+        <tr>
+            <td>${page.name}</td>`;
+        mdContent += (page.checkbox == true) ? `<td><p>U+2705</p></td>
+        </tr>` : `<td></td>
+        </tr>`;
     }
+    mdContent += `
+    </tbody>
+    </table>`;
+      
+    fs.writeFileSync("README.md", mdContent, "utf-8", (e) => {
+        console.log(e);
+    });
 })();
